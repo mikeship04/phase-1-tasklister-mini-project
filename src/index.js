378b611576866c3ed22caf7315aba705116bc7cb
Mike Shippy <mikeship04@gmail.com>
2022-06-09 13:22:29 -0700

    first push of tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.new_task.value)
+    form.reset()
+  })
+
 });
+
+function buildToDo (todo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = ' X'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
+// function handleDelete(e){
+//   e.target.parentNode.remove()
+// }
+
+// function addEventListenerToDeletebuttons(){
+//   let buttons = document.getElementsByClassName('delete_btn')
+//   for(let btn of buttons){
+//     btn.addEventListener('click', handleDelete)
+//   }
+// }
+
+// addEventListenerToDeletebuttons()
+
